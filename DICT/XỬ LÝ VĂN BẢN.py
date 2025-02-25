fbe6db969036d40f50a786f49c1cfedbc36a774f
tiendat8438 <tiendat9320@gmail.com>
2025-02-25 20:55:41 +0700

    basic

---
+++ DICT/XỬ LÝ VĂN BẢN.py
@@ -0,0 +1,20 @@
+import sys
+import re
+
+def format_string(text):
+    lines = re.split(r'[.?!]', text)
+    res = []
+    for line in lines:
+        words = line.strip().split()
+        if words:
+            formatted_line = ' '.join(words).capitalize()
+            res.append(formatted_line)
+    return res
+
+if __name__ == "__main__":
+    text = ""
+    for line in sys.stdin:
+        text += line.strip().split()
+    sentences = format_string(text)
+    for line in sentences:
+        print(line)
